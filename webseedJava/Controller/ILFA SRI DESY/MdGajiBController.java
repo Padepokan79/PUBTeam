b61ce59eaf9c988c2274f31533c0e38079d4cb87
IlfaSriDesy <ilfasridesy06121997@gmail.com>
2017-10-24 15:47:40 +0700

    ILFA add controller

---
+++ webseedJava/Controller/ILFA SRI DESY/MdGajiBController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdGajiB;
+import core.controllers.CRUDController;
+
+public class MdGajiBController extends  CRUDController<MdGajiB>{
+
+}
---
+++ webseedJava/Controller/ILFA SRI DESY/MdJenisKekuranganGajiController.java
@@ -0,0 +1,9 @@
+package app.controllers.api.masterdata;
+
+
+import app.models.MdJenisKekuranganGaji;
+import core.controllers.CRUDController;
+
+public class MdJenisKekuranganGajiController extends  CRUDController<MdJenisKekuranganGaji>{
+
+}
