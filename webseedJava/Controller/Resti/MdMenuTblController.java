5d15f9d5be9cb5aebedb966e2feaaa27d40ccb0c
restisiskadevi <restisiskadeviaction.10@gmail.com>
2017-10-24 15:55:38 +0700

    controller resti

---
+++ webseedJava/Controller/Resti/MdMenuTblController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdMenuTbl;
+import core.controllers.CRUDController;
+
+public class MdMenuTblController extends CRUDController<MdMenuTbl>{
+
+}
