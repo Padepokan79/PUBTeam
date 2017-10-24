efba45d438f9a0f1927c9f2d66d660910157a7b3
cucuyuni17 <cucuyuni17@gmail.com>
2017-10-24 15:56:15 +0700

    controller cucu

---
+++ webseedJava/Controller/Cucu Yuni Yulianti/MdMstTht0TblController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdMstTht0Tbl;
+import core.controllers.CRUDController;
+
+public class MdMstTht0TblController extends CRUDController<MdMstTht0Tbl>{
+
+}
