0fc39dfdbcb35e6fcc2435bffaa236e0da7c8901
sopianipin <sopianipin7@gmail.com>
2017-10-24 15:17:18 +0700

    add java file

---
+++ webseed/Ipin Sopian/MdJenisDidik.java
@@ -0,0 +1,11 @@
+package app.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.IdName;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("jenis_didik")
+@IdName("jnsdidik")
+public class MdJenisDidik extends Model{
+
+}
