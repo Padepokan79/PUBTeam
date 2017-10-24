4ff3b4647a84ea725a9474f21345d7dcb5b57259
jejenZenalMutaqin <jejen.zenal96@gmail.com>
2017-10-24 15:55:53 +0700

    jejen Ok

---
+++ webseedJava/Controller/JEJEN ZENAL MUTAQIN/MdFgajiBaru1Controller.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdFgajiBaru1;
+import core.controllers.CRUDController;
+
+public class MdFgajiBaru1Controller extends CRUDController<MdFgajiBaru1>{
+
+}
---
+++ webseedJava/Controller/JEJEN ZENAL MUTAQIN/MdHistorisPendidikanController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdHistorisPendidikan;
+import core.controllers.CRUDController;
+
+public class MdHistorisPendidikanController extends CRUDController<MdHistorisPendidikan>{
+
+}
