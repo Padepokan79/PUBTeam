42bc36131e95827f8084864ffd5c57df4138f55b
UlilHidayah <ulilhidayah1998@gmail.com>
2017-10-24 15:54:00 +0700

    ulil controller

---
+++ webseedJava/Controller/Ulil Hidayah/MdMstpegawai04Controller.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdMstpegawai04;
+import core.controllers.CRUDController;
+
+public class MdMstpegawai04Controller extends CRUDController<MdMstpegawai04>{
+
+}
