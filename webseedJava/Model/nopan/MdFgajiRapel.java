c940c733ffb188fd6caa87b6173f6678c3bf0a0d
nopan <nopan2327@gmail.com>
2017-10-24 15:49:12 +0700

    tambah

---
+++ webseedJava/Model/nopan/MdFgajiRapel.java
@@ -0,0 +1,11 @@
+package app.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.IdName;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("fgaji_rapel")
+@IdName("kdpangkat")
+public class MdFgajiRapel extends Model{
+
+}
