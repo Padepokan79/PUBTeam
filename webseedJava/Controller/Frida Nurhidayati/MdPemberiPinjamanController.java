8b2dfe264e81b4b57e52d83ba00027af6d82820b
FridaNurhidayati <fridanurhidayati8@gmail.com>
2017-10-24 15:52:59 +0700

    add

---
+++ webseedJava/Controller/Frida Nurhidayati/MdPemberiPinjamanController.java
@@ -0,0 +1,7 @@
+package app.controllers.api.masterdata;
+import app.models.MdPemberiPinjaman;
+import core.controllers.CRUDController;
+
+public class MdPemberiPinjamanController extends CRUDController<MdPemberiPinjaman> {
+
+}
