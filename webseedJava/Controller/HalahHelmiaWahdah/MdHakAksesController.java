cb92b6c83ac12bbdb5bbebe05b5191a11f57ad18
halahhelmia <halahhelmia88@gmail.com>
2017-10-24 15:54:01 +0700

    controller

---
+++ webseedJava/Controller/HalahHelmiaWahdah/MdHakAksesController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdHakAkses;
+import core.controllers.CRUDController;
+
+public class MdHakAksesController extends CRUDController<MdHakAkses>{
+
+}
