267ee4e69d58452e57f7c628ad42d6df35f8f026
Nurikhsan <ikhsanhikari29@gmail.com>
2017-10-24 15:59:34 +0700

    COntroller And Model Ikhsan

---
+++ webseedJava/Model/Nurikhsan/MdAgamatbl.java
@@ -0,0 +1,9 @@
+package app.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("AGAMA_TBL")
+public class MdAgamatbl extends Model {
+
+}
---
+++ webseedJava/Model/Nurikhsan/MdFspt.java
@@ -0,0 +1,9 @@
+package app.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.IdName;
+import org.javalite.activejdbc.annotations.Table;
+@Table("f_spt")
+public class MdFspt extends Model{
+
+}
