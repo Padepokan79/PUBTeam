b0d1d3f5ca901f528d9583ee84d1faf10037b3fa
dasiah07 <dasiahdasiah07@gmail.com>
2017-10-24 15:47:38 +0700

    Tugas Controller Dasiah ready

---
+++ webseedJava/Controller/Dasiah/MdJadualHutangController.java
@@ -0,0 +1,7 @@
+package app.controllers.api.masterdata;
+import app.models.MdJadualHutang;
+import core.controllers.CRUDController;
+
+public class MdJadualHutangController extends CRUDController<MdJadualHutang> {
+
+}
