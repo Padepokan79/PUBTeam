895ad6a69d54d1b4de5d82c45b1d943fb799f6fa
RiyanTurmudi <riyanturmudzi11@gmail.com>
2017-10-24 15:35:42 +0700

    Add Riyan Controller

---
+++ webseedJava/Controller/Riyan/MdPendidikantblController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdPendidikantbl;
+import core.controllers.CRUDController;
+
+public class MdPendidikantblController extends CRUDController<MdPendidikantbl> {
+
+}
---
+++ webseedJava/Controller/Riyan/MdProvincesController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdProvinces;
+import core.controllers.CRUDController;
+
+public class MdProvincesController extends CRUDController<MdProvinces> {
+
+}
