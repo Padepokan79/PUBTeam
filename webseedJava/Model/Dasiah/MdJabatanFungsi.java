4e898c3798ed0de3676556ab6799130c8f549e19
dasiah07 <dasiahdasiah07@gmail.com>
2017-10-24 15:49:21 +0700

    Tugas Models dasiah ready

---
+++ webseedJava/Model/Dasiah/MdJabatanFungsi.java
@@ -0,0 +1,10 @@
+package app.models;
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.IdName;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("jabatan_fungsi")
+@IdName("KDFUNGSI")
+public class MdJabatanFungsi extends Model {
+
+}
