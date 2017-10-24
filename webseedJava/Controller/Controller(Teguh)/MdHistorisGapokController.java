b4cd66fcba4de38a1eb357974368926bd2704bc0
Sadega345 <katateguh345@gmail.com>
2017-10-24 15:44:54 +0700

    beres

---
+++ webseedJava/Controller/Controller(Teguh)/MdHistorisGapokController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdHistorisGapok;
+import core.controllers.CRUDController;
+
+public class MdHistorisGapokController extends CRUDController<MdHistorisGapok>{
+
+}
