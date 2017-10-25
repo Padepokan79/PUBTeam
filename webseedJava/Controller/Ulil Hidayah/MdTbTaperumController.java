fcc82a56f810546c59869b240d5b16d92d00862a
UlilHidayah <ulilhidayah1998@gmail.com>
2017-10-25 13:39:35 +0700

    ulil controller

---
+++ webseedJava/Controller/Ulil Hidayah/MdTbTaperumController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdTbTaperum;
+import core.controllers.CRUDController;
+
+public class MdTbTaperumController extends CRUDController<MdTbTaperum> {
+
+}
