9d456f6238d812f028d90441f8ab5b556f585f50
UlilHidayah <ulilhidayah1998@gmail.com>
2017-10-25 13:40:37 +0700

    ulil models

---
+++ webseedJava/Model/Ulil Hidayah/MdBupIndTbl.java
@@ -0,0 +1,12 @@
+package app.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.IdName;
+import org.javalite.activejdbc.annotations.Table;
+
+
+@Table("bup_ind_tbl")
+@IdName("NIP")
+public class MdBupIndTbl extends Model{
+
+}
