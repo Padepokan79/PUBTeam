0693010bf620090962dc81dac53ef61fdabdf1b8
RiyanTurmudi <riyanturmudzi11@gmail.com>
2017-10-25 14:38:58 +0700

    Riyan Add Controller

--- webseedJava/Controller/Riyan/MdProvincesController.java
+++
@@ -1,8 +0,0 @@
-package app.controllers.api.masterdata;
-
-import app.models.MdProvinces;
-import core.controllers.CRUDController;
-
-public class MdProvincesController extends CRUDController<MdProvinces> {
-
-}
