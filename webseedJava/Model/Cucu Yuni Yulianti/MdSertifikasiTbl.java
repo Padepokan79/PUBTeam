a5db867765122af421b2fe5a087a596b827df2a3
cucuyuni17 <cucuyuni17@gmail.com>
2017-10-25 13:57:05 +0700

    Cucu Model

--- webseedJava/Model/Cucu Yuni Yulianti/MdSertifikasiTbl.java
+++ webseedJava/Model/Cucu Yuni Yulianti/MdSertifikasiTbl.java
@@ -3,9 +3,8 @@ package app.models;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.IdName;
 import org.javalite.activejdbc.annotations.Table;
-
-@Table("filebln")
-@IdName("CURR_MM")
-public class MdFileBln extends Model{
+@Table("sertifikasi_tbl")
+@IdName("NIP")
+public class MdSertifikasiTbl extends Model {
 
 }
--- webseedJava/Model/Cucu Yuni Yulianti/MdUserAksesCopy.java
+++ webseedJava/Model/Cucu Yuni Yulianti/MdUserAksesCopy.java
@@ -3,8 +3,8 @@ package app.models;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.CompositePK;
 import org.javalite.activejdbc.annotations.Table;
-@Table("jabatan_tbl")
-@CompositePK({"TMTJABATAN", "KDJABATAN"})
-public class MdJabatanTbl extends Model{
+@Table("user_akses_copy")
+@CompositePK({"pemakai", "kdskpd"})
+public class MdUserAksesCopy extends Model{
 
 }
