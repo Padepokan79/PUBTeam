8ba9fa1b03535fb3cab6ddc1d39e0947b5af43a4
jejenZenalMutaqin <jejen.zenal96@gmail.com>
2017-10-25 13:58:20 +0700

    jejen model OK

--- webseedJava/Model/JEJEN ZENAL MUTAQIN/MdSeluma04.java
+++ webseedJava/Model/JEJEN ZENAL MUTAQIN/MdSeluma04.java
@@ -3,8 +3,8 @@ package app.models;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.CompositePK;
 import org.javalite.activejdbc.annotations.Table;
-@Table("otostruk")
-@CompositePK({"KDSSBP","KDSKPD","KDSATKER"})
-public class MdOtostruk extends Model{
+@Table("seluma04")
+@CompositePK({"FD_SATKER1","FD_SATKER2","FD_KDGIAT"})
+public class MdSeluma04 extends Model{
 
 }
--- webseedJava/Model/JEJEN ZENAL MUTAQIN/MdTmutasiBatch.java
+++ webseedJava/Model/JEJEN ZENAL MUTAQIN/MdTmutasiBatch.java
@@ -3,8 +3,8 @@ package app.models;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.CompositePK;
 import org.javalite.activejdbc.annotations.Table;
-@Table("historis_pendidikan")
-@CompositePK({"NIP","TMTAWAL","TMTAKHIR"})
-public class MdHistorisPendidikan extends Model {
+@Table("t_mutasi_batch")
+@CompositePK({"Nip","KdSkpd_l","KdSatker_l"})
+public class MdTmutasiBatch extends Model{
 
 }
