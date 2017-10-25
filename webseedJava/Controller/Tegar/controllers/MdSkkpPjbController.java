a30c927839c51416595e3034340232af0c9b3e01
Tegar Budi Septian <tegarbudiseptian@gmail.com>
2017-10-25 13:41:55 +0700

    Controllers, Tegar

---
+++ webseedJava/Controller/Tegar/controllers/MdSkkpPjbController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdSkkpPjb;
+import core.controllers.CRUDController;
+
+public class MdSkkpPjbController extends CRUDController<MdSkkpPjb> {
+
+}
---
+++ webseedJava/Controller/Tegar/controllers/MdTrZakatTblController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdTrZakatTbl;
+import core.controllers.CRUDController;
+
+public class MdTrZakatTblController extends CRUDController<MdTrZakatTbl> {
+
+}
