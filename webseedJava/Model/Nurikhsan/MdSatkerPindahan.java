845a183c67b5cdfbbc201605b0f68867a8859e6a
Nurikhsan <ikhsanhikari29@gmail.com>
2017-10-25 14:46:51 +0700

    Model

---
+++ webseedJava/Model/Nurikhsan/MdSatkerPindahan.java
@@ -0,0 +1,10 @@
+package app.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.IdName;
+import org.javalite.activejdbc.annotations.Table;
+@Table("satker_pindahan")
+@IdName("KDSATKER_L")
+public class MdSatkerPindahan extends Model {
+
+}
