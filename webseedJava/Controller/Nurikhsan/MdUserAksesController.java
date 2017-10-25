5ad87e2c8c5732d619e3f40fe6c0c837e5a4b4eb
Nurikhsan <ikhsanhikari29@gmail.com>
2017-10-25 14:45:31 +0700

    Controller

---
+++ webseedJava/Controller/Nurikhsan/MdUserAksesController.java
@@ -0,0 +1,8 @@
+package app.controllers.api.masterdata;
+
+import app.models.MdUserAkses;
+import core.controllers.CRUDController;
+
+public class MdUserAksesController extends CRUDController<MdUserAkses>{
+
+}
--- webseedJava/Model/Nurikhsan/MdFspt.java
+++ webseedJava/Model/Nurikhsan/MdFspt.java
@@ -4,6 +4,7 @@ import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.IdName;
 import org.javalite.activejdbc.annotations.Table;
 @Table("f_spt")
+@IdName("nip")
 public class MdFspt extends Model{
 
 }
